594407fbd56ea3a7be1fb98c0d342339c9696148
natthawutGitZ <natthawut.niny@gmail.com>
2025-04-16 18:18:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
